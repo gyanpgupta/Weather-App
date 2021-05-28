05671ff266c88394fadc01c3b531ec88efdbcdec
gyanpgupta <guptagyan58@gmail.com>
2021-05-28 14:51:41 +0530

    updated readme and also added comments in backend code.

--- backend/README.md
+++ backend/README.md
@@ -1,11 +1,39 @@
-### `npm install
+### Packages Installation
 
-Run this command for installing the packages
+Run `npm install` command for installing the required packages.
 
-### Create .env file
+### Setup Environment
 
-Copy the .env.example code and paste into .env file for configuring the environment
+Create the `.env` file and copy the _.env.example_ file code and paste into **.env** file for configuring the environment
 
-### `npm start`
+### Install Redis in system.
 
-Run this command for starting the server
+> If Redis is not installed in the system then run the below command for installing the Redis else you can directly start the server.
+
+`wget http://download.redis.io/redis-stable.tar.gz`
+
+`tar xvzf redis-stable.tar.gz`
+
+`cd redis-stable`
+
+`make`
+
+**\*Confirm installation by running command\*\***
+
+`make test`
+
+> You can then copy redis to your path by running the below command.
+
+`sudo make install`
+
+> To confirm that redis has been properly setup, start the redis server by running
+
+`redis-server`
+
+### Redis Server
+
+Run `redis-server` command for starting the redis server process.
+
+### Server Starting
+
+Run `npm start` command for starting the server
--- backend/controllers/Weather.ts
+++ backend/controllers/Weather.ts
@@ -25,11 +25,13 @@ const list: any = async (req: express.Request, res: express.Response) => {
       query: { searchValue = '' },
     } = req;
 
-    const LOCATION_API: any = process.env.LOCATION_API;
-    const HOURLY_API: any = process.env.HOURLY_API;
-    const WEEKLY_API: any = process.env.WEEKLY_API;
+    const LOCATION_API: any = process.env.LOCATION_API; // location api url
+    const HOURLY_API: any = process.env.HOURLY_API; // hourly data api url
+    const WEEKLY_API: any = process.env.WEEKLY_API; // weekly data api url
 
     const value: string | any = searchValue;
+    // To check user entered text is place name or lat/long
+    // if matches means user entered the lat/long of the place
     var matches = value.match(/\d+/g);
 
     await axios
@@ -46,13 +48,16 @@ const list: any = async (req: express.Request, res: express.Response) => {
       )
       .then(async (cityResult) => {
         if (cityResult && cityResult.data && cityResult.data[0]) {
-          const key = cityResult.data[0].Key;
-          const localizedName: string = cityResult.data[0].LocalizedName;
+          const key = cityResult.data[0].Key; // Place key
+
+          const localizedName: string = cityResult.data[0].LocalizedName; // Place name
+
           const localizedState: string =
-            cityResult.data[0].AdministrativeArea.LocalizedName;
+            cityResult.data[0].AdministrativeArea.LocalizedName; // Place state name
 
           // Check the redis store for the data first
           await client.get(`weather-${key}`, async (err: any, result: any) => {
+            // if data found in cache then send the response to client.
             if (result) {
               console.log('result cache');
               const { hourlyData, weeklyData } = JSON.parse(result);
@@ -67,20 +72,25 @@ const list: any = async (req: express.Request, res: express.Response) => {
             }
             console.log('without cache');
 
+            // Runs only if data is not available in cache.
             await axios
               .get(`${HOURLY_API.replace(':key', key)}`)
               .then(async (response: object | any) => {
+                // hourly weather data response
                 await axios
                   .get(`${WEEKLY_API.replace(':key', key)}`)
                   .then(async (result: object | any) => {
+                    // weekly weather data response
+
                     const cacheData = {
+                      // cache object with hourly and weekly data
                       hourlyData: response.data,
                       weeklyData: result.data,
                     };
 
                     // save the record in the cache for subsequent request
                     client.setex(
-                      `weather-${key}`,
+                      `weather-${key}`, // place name key it's unique
                       1440,
                       JSON.stringify(cacheData)
                     );
